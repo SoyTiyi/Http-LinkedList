182ef726287efc19142b4a8770e83fce8c825c39
SoyTiyi <santiago.marmartinez@gmail.com>
2021-01-30 21:21:23 -0500

    Test Push

--- src/main/java/edu/escuelaing/arep/app/Spark/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/app/Spark/SparkWebApp.java
@@ -6,7 +6,7 @@ public class SparkWebApp {
     
     public static void main(String[] args){
         port(getPort());
-        get("/hello", (req,res) -> "Hello Heroku");
+        get("/hello", (req,res) -> "Hello Heroku - prueba");
     }
 
     static int getPort(){
